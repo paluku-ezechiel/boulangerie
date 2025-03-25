72e4bd7e97ef52e6c7eaa123b6bd3b2068ce9191
palukuezechiel@gmail.com <palukuezechiel@gmail.com>
2025-03-25 19:30:23 +0100

    ajout de fonctionnalite javascript et autre fonctionnalite css

---
+++ js/function.js
@@ -0,0 +1,21 @@
+var burgerIcon  = document.querySelector('.burger-icon');
+var navLogo     = document.querySelector('nav#menu .logo');
+var listes          = document.querySelectorAll('nav#menu ul li');
+
+var listenerFunction = {
+    openNav : () => {
+        navLogo.style.display = 'block';
+        for (let index = 0; index < listes.length; index++) {
+            const elt = listes[index];
+            elt.style.display      = 'block';
+        }
+    },
+
+    closeNav : () => {
+        burgerIcon.style.display = 'none'
+    }
+}
+
+var setupListener = () => {
+        burgerIcon.addEventListener('mouseover', listenerFunction.openNav);
+    }
