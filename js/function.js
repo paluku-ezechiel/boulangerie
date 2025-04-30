157ee23f950346c7ccfba99baf9cfb0dcf1e4e16
palukuezechiel@gmail.com <palukuezechiel@gmail.com>
2025-04-30 13:02:41 +0200

    correction sur layout

--- js/function.js
+++ js/function.js
@@ -1,13 +1,13 @@
 var burgerIcon  = document.querySelector('.burger-icon');
 var navLogo     = document.querySelector('nav#menu .logo');
-var listes          = document.querySelectorAll('nav#menu ul li');
+var listes      = document.querySelectorAll('nav#menu ul li');
 
 var listenerFunction = {
     openNav : () => {
         navLogo.style.display = 'block';
         for (let index = 0; index < listes.length; index++) {
             const elt = listes[index];
-            elt.style.display      = 'block';
+            elt.style.display = 'block';
         }
     },
 
